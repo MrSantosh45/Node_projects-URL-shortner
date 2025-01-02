91f94c00a25d407b1a813df9f511662e3c5a1233
Santosh Channamallappa Kumbar <139158200+MrSantosh45@users.noreply.github.com>
2025-01-02 13:31:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,21 +13,6 @@ app.use(express.json());
 
 app.use("/url",urlRoute);
 
-// app.get("/:shortId",async(req,res)=>{
-//     const shortId=req.params.shortId;
-//     const entry=await URL.findOneAndUpdate({
-//         shortId
-//     },{
-//         $push:{
-//             visitHistory:{
-//                 timestamp:Date.now()
-//             },
-//         },
-//     });
-//     res.redirect(entry.redirectURL);
-// })
-
-
 const PORT=8001;
 
 app.listen(PORT,()=>console.log(`Server is running on post ${PORT}`));
